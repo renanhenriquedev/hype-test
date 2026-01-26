fa9213c8d91de9044bd0fc015d15d5047d358dc3
Renan Henrique <renanhenrique20222@gmail.com>
2026-01-26 20:23:20 -0300

    Fix Cloud Run listen + remove/adjust healthcheck

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
 import { AuthModule } from './auth/auth.module';
 import { VideosModule } from './videos/videos.module';
@@ -11,5 +13,7 @@ import { VideosModule } from './videos/videos.module';
     AuthModule,
     VideosModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://91d8e0b359de.ngrok-free.app/'],
+    origin: [
+      'http://localhost:5173',
+      'https://91d8e0b359de.ngrok-free.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(process.env.PORT || 4001);
+  const port = Number(process.env.PORT) || 4001;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
